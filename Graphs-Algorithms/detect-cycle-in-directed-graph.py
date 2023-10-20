f02a2c0a5035459f3bdbbec0e41d872c9e199bb9
Athsara Fernando <126111010+AthsaraFernando@users.noreply.github.com>
2023-10-20 22:05:28 +0530

    Update detect-cycle-in-directed-graph.py

--- Graphs-Algorithms/detect-cycle-in-directed-graph.py
+++ Graphs-Algorithms/detect-cycle-in-directed-graph.py
@@ -8,35 +8,37 @@ Given a directed graph, determine whether it contains any cycle. Implement a Pyt
 def is_cyclic(graph):
     visited = set()
     rec_stack = set()
+    cycle = []
 
-    def dfs(node):
+    def dfs(node, path):
         if node in rec_stack:
-            return True
+            return True, path + [node]
         if node in visited:
-            return False
+            return False, path
 
         visited.add(node)
         rec_stack.add(node)
 
         for neighbor in graph.get(node, []):
-            if dfs(neighbor):
-                return True
+            has_cycle, cycle_path = dfs(neighbor, path + [node])
+            if has_cycle:
+                return True, cycle_path
 
         rec_stack.remove(node)
-        return False
+        return False, path
 
     for node in graph:
-        if dfs(node):
+        has_cycle, cycle_path = dfs(node, [])
+        if has_cycle:
+            print("Cycle Found:", " -> ".join(cycle_path))
             return True
     return False
 
 # Test the function with a sample graph
 graph = {'A': ['B', 'C'],
-         'B': ['D'],
+         'B': ['A','D'],
          'C': ['E'],
          'D': ['C'],
          'E': []}
 
 print(is_cyclic(graph))  # Output: True
-
-
